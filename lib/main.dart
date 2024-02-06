9fbcb34a1033ad172b7cf95848b63ae92f4b8fe0
AlejandroCas26 <156716033+AlejandroCas26@users.noreply.github.com>
2024-02-05 20:38:13 -0600

    navegando al segundo link con el push

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,13 @@
-¿class FirstRoute extends StatelessWidget {
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    title: 'Navigation Basics',
+    home: FirstRoute(),
+  ));
+}
+
+class FirstRoute extends StatelessWidget {
   const FirstRoute({super.key});
 
   @override
@@ -11,29 +20,11 @@
         child: ElevatedButton(
           child: const Text('Open route'),
           onPressed: () {
-            // Navigate to second route when tapped.
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SecondRoute()),
+            );
           },
-        ),
-      ),
-    );
-  }
-}
-
-class SecondRoute extends StatelessWidget {
-  const SecondRoute({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Second Route'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Navigate back to first route when tapped.
-          },
-          child: const Text('Go back!'),
         ),
       ),
     );
